04e7b111cc4b5764eb78d91c8eb2aea14535fc56
John Tex <johnrteixeira@gmail.com>
2022-01-04 13:52:43 -0500

    working on external properties in spring

--- src/main/java/com/johnrteixeira/sfgdi/SfgDiApplication.java
+++ src/main/java/com/johnrteixeira/sfgdi/SfgDiApplication.java
@@ -1,6 +1,7 @@
 package com.johnrteixeira.sfgdi;
 
 import com.johnrteixeira.sfgdi.controllers.*;
+import com.johnrteixeira.sfgdi.datasource.FakeDataSource;
 import com.johnrteixeira.sfgdi.services.PrototypeBean;
 import com.johnrteixeira.sfgdi.services.SingletonBean;
 import org.springframework.boot.SpringApplication;
@@ -48,6 +49,11 @@ public class SfgDiApplication {
 		System.out.println(prototypeBean1.getMyScope());
 		PrototypeBean prototypeBean2 = ctx.getBean(PrototypeBean.class);
 		System.out.println(prototypeBean2.getMyScope());
+
+		FakeDataSource fakeDataSource = ctx.getBean(FakeDataSource.class);
+		System.out.println(fakeDataSource.getUsername());
+		System.out.println(fakeDataSource.getPassword());
+		System.out.println(fakeDataSource.getJdbcurl());
 	}
 
 }
--- src/main/java/com/johnrteixeira/sfgdi/config/GreetingServiceConfig.java
+++ src/main/java/com/johnrteixeira/sfgdi/config/GreetingServiceConfig.java
@@ -3,16 +3,30 @@ package com.johnrteixeira.sfgdi.config;
 import com.johnrteixeira.pets.DogPetService;
 import com.johnrteixeira.pets.PetService;
 import com.johnrteixeira.pets.PetServiceFactory;
+import com.johnrteixeira.sfgdi.datasource.FakeDataSource;
 import com.johnrteixeira.sfgdi.repositories.EnglishGreetingRepository;
 import com.johnrteixeira.sfgdi.repositories.EnglishGreetingRepositoryImpl;
 import com.johnrteixeira.sfgdi.repositories.SpanishGreetingRepository;
 import com.johnrteixeira.sfgdi.repositories.SpanishGreetingRepositoryImpl;
 import com.johnrteixeira.sfgdi.services.*;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.*;
 
 @ImportResource("classpath:sfgdi-config.xml")
 @Configuration
 public class GreetingServiceConfig {
+
+    @Bean
+    FakeDataSource fakeDataSource(@Value("${guru.username}") String username,
+                                  @Value("${guru.password}") String password,
+                                  @Value("${guru.jdbcurl}") String jdbcurl){
+        FakeDataSource fakeDataSource = new FakeDataSource();
+        fakeDataSource.setUsername(username);
+        fakeDataSource.setPassword(password);
+        fakeDataSource.setJdbcurl(jdbcurl);
+
+        return fakeDataSource;
+    }
 
     @Bean
     PetServiceFactory petServiceFactory(){
